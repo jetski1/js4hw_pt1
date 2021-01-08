f83825f697bafecfa35ef8acdc81d9b56f2d28b2
Bekzod <76199273+jetski1@users.noreply.github.com>
2021-01-08 17:09:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function age() {
     return result
 }
 let result = age()
-irth = !isNaN(birth) && birth > 0 ? birth :  alert('Ошибка, вы на год рождения ввели отрицательное число или не число ')
+birth = !isNaN(birth) && birth > 0 ? birth :  alert('Ошибка, вы на год рождения ввели отрицательное число или не число ')
 year= !isNaN(year) && year > 0 ? year : alert('Ошибка, вы на нынешний год ввели отрицательное число или не число ')
 console.info('Ваше имя ' + usName +' ваш возраст ' + result)
 alert( 'Ваше имя ' + usName +' ваш возраст ' + result ) 
